7eb7e4f03f1bcc04e9f0061bd558bd5f4f55ced9
manimani708 <26115001@stn.nitech.ac.jp>
2017-07-13 01:01:06 +0900

    プロジェクト作成

---
+++ Assets/Scripts/TournamentData.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TournamentData :MonoBehaviour
+{
+
+    #region データ変数
+    private string tournamentName = "";
+    private string myDeckName = "";
+    private List<BattleResult> battleResults = new List<BattleResult>();
+    #endregion
+
+    public void SetTournamentName(InputField inputField)
+    {
+        tournamentName = inputField.text;
+    }
+
+    public void SetMyDeckName(InputField inputField)
+    {
+        myDeckName = inputField.text;
+    }
+
+
+}
+
+public class BattleResult
+{
+    #region データ変数
+    private string opponentDeckName = "";
+    private int[] sideAmount = new int[2];
+    #endregion
+}
